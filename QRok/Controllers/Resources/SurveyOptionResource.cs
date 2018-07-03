0d1137415f082b3d94710e47933b4d94bf2a5b49
Aleksandr Spiridonov <sasha.aos@gmail.com>
2018-07-02 20:47:31 -0400

    Added resources for Survey and SurveyOption models

---
+++ QRok/Controllers/Resources/SurveyOptionResource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QRok.Controllers.Resources
+{
+    public class SurveyOptionResource
+    {
+
+        public int OptionNumber { get; set; }
+
+        public string Title { get; set; }
+
+        public int Count { get; set; }
+
+    }
+}
---
+++ QRok/Controllers/Resources/SurveyResource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QRok.Controllers.Resources
+{
+    public class SurveyResource
+    {
+        public int Id { get; set; }
+
+        public Guid Guid { get; set; }
+
+        public string Title { get; set; }
+
+        public IList<SurveyOptionResource> SurveyOptions { get; set; }
+
+        public DateTime CloseDateTime { get; set; }
+        public DateTime DeleteDateTime { get; set; }
+
+        public SurveyResource()
+        {
+            SurveyOptions = new List<SurveyOptionResource>();
+        }
+    }
+}
